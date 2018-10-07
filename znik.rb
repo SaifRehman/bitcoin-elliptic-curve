11dcb48b2e0b3b696002bd341e37f2301b113bd5
SaifRehman <s4saif.121@gmail.com>
2018-10-08 01:09:26 +0400

    added zero knowledge implementation

--- znik.rb
+++ znik.rb
@@ -6,12 +6,6 @@ $xcoordinate = 55066263022277343669578718895168534326250603453777594175500187360
 $ycoordinate = 32670510020758816978083085130507043184471273380659243275938904335757337482424
 $privKey = "A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3"
 $point  = Array.new([$xcoordinate,$ycoordinate])
-$hashOfThingToSign = 86032112319101611046176971828093669637772856272773459297323797145286374828050 
-$randLow =  100
-$randHigh = 999
-$m = 2**32
-$a = 1103515245
-$c = 12345
 
 def dectobin(var)
     return var.to_s(2)
@@ -36,26 +30,6 @@ end
 def generateSHA256(var)
     q = Digest::SHA256.hexdigest var
     return q.to_i(16)
-end
-
-def randomNumberGeneration ()
-    seed = rand $randLow..$randHigh
-    seed = seed.to_f
-    iter = rand $randLow..$randHigh
-    iter = iter.to_i
-    lastVal = 0
-    counter = 0
-    while counter < iter
-        seed = (($a * seed) + $c) % $m
-        counter = counter + 1
-        lastVal = seed/$m
-    end
-    numberOfChar = ((lastVal.to_s).length)-2
-    numberOfChar = rand 2..numberOfChar
-    numberOfChar = numberOfChar.to_i
-    numberOfChar = 10**numberOfChar
-    # puts (lastVal*numberOfChar)
-    return (lastVal*numberOfChar).to_i
 end
 
 def modInverse (var,n)
@@ -97,53 +71,20 @@ def EccMultiply(genPoint,scalarHex)
     q=genPoint
     for i in 1..scalarBin.length-1
         q=ECdouble(q) 
-        # puts "DUB", q[0]
         if scalarBin[i] == '1'
             q=ECadd(q,genPoint)
-            # puts "ADD", q[0]
         end
     end
     return [q]
 end
 
-def SignatureGeneration(genPoint,randomNumber,hashOfThingToSign)
-    q = EccMultiply(genPoint,randomNumber)
-    xRandSignPoint = q[0][0]
-    yRandSignPoint = q[0][1]
-    r = xRandSignPoint % $numberOfPointsInFeild;
-    s = ((hashOfThingToSign + (r*hextodec($privKey)))*(modInverse(randomNumber,$numberOfPointsInFeild))) % $numberOfPointsInFeild
-    return [r,s]
-end
-
-def SignatureVerification(s,r,hashOfThingToSign,publicKey)
-    w = modInverse(s,$numberOfPointsInFeild)
-    point1 = EccMultiply($point,((hashOfThingToSign * w) % $numberOfPointsInFeild))
-    point2 = EccMultiply(publicKey,((r*w) % $numberOfPointsInFeild))
-    q  = ECadd(point1[0],point2[0])
-    x = q[0]
-    y = q[1]
-    if(x === r)
-        return true
-    else
-        return false
-    end
-end
 
 publicKey = EccMultiply($point,hextodec($privKey))
 
 v = 0xd90688c575782cb4970514c7623fee2d6045e5d3aa48507beb3c9804453718d2
 v1 = EccMultiply($point, v)
-c = 100
+c = generateSHA256($point[0].to_s + v.to_s + publicKey[0].to_s )
 r = (v - hextodec($privKey) * c) % $numberOfPointsInFeild
 final = ECadd(EccMultiply($point,r)[0],EccMultiply(publicKey[0],c)[0])
 puts (v1)
-puts (final)
-# puts "the uncompressed public key (HEX):";
-# puts "04" + publicKey[0][0].to_s(16) + publicKey[0][1].to_s(16); 
-# sha256hash = generateSHA256("saif")
-# r,s= SignatureGeneration($point,(randomNumberGeneration()),sha256hash)
-# result = SignatureVerification(s,r,sha256hash,publicKey[0])
-# puts result
-# puts publicKey
-# puts "the uncompressed public key (HEX):";
-# puts "04" + publicKey[0][0].to_s(16) + publicKey[0][1].to_s(16);
+puts (final) # if v1 and final are same point, this mean verifer has sucesfully verified the statement
